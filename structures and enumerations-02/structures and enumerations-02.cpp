0b3d135af88c5a5fc2a1f7de60aa3d5521e8ed48
Rom <kornev.roma1881@gmail.com>
2023-09-24 15:01:35 +0300

    Добавьте файлы проекта.

---
+++ structures and enumerations-02/structures and enumerations-02.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <Windows.h> 
+
+struct account
+{
+    int accNumber;
+    std::string name;
+    float cash;
+    
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Введите номер счета: ";
+    int accNumber;
+    std::cin >> accNumber;
+    std::cout << "Введите имя владельца: ";
+    std::string name;
+    std::cin >> name;
+    std::cout << "Введите баланс: ";
+    float cash;
+    std::cin >> cash;
+    std::cout << "Введите новый баланс: ";
+    float cash1;
+    std::cin >> cash1;
+
+    account account1 = { accNumber, name, cash };
+
+    account1.cash = cash1;
+
+    std::cout << "Ваш счет: " << account1.name << ", " << account1.accNumber << ", "
+        << account1.cash;
+    }
+
